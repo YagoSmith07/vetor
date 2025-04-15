fc141f6c63facc763d2c0b2cb8d486e7c302a427
YagoSmith07 <yagosmith04@gmail.com>
2025-04-15 10:48:16 -0300

    criação do index/calcular exercicio 2

---
+++ calcular.php
@@ -0,0 +1,7 @@
+<?php
+$n = $_POST ["g"];
+$n = $_POST ["c"];
+ $n = rand(0, $n);
+ 
+    echo("O numero aleatório é: ".$n);
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home page</title>
+</head>
+<body>
+    digite um número:
+   <fieldset>
+
+    <form method="post"  action="calcular.php">
+    <input type="text" name= "c" > <br> </br>
+    <input type="submit" name= "g" value="gerar">
+   </fieldset>
+   
+</body>
+
+</body>
+</html>
